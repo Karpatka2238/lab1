a2ad2fb62d3ba62129804f9a587b16760a78ad84
Karpatka2238 <101676223+Karpatka2238@users.noreply.github.com>
2022-05-22 17:50:59 +0200

    punkt 8

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -5,7 +5,7 @@ package pl.lublin.wsei.java.cwiczenia;
 import java.lang.Math;
 
 
-
+/*
 public class Main {
     public static void main(String[] args) {
         // Pkt 7.
@@ -42,4 +42,29 @@ public class Main {
 
 
 
+}
+*/
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 =0, num2 =0;
+
+        do {
+            System.out.print("Podaj pierwszą liczbę: ");
+            num1 = input.nextInt();
+            if (num1 != 0) {
+                System.out.print("Podaj drugą liczbę: ");
+                num2 = input.nextInt();
+            }
+            System.out.printf("Wynik dodawania %d + %d = %d ", num1, num2, num1 + num2);
+
+
+        } while ( num1 !=0 && num2!=0 );
+
+    }
 }
