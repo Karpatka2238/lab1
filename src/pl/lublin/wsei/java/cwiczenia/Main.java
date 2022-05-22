f27bb0573a95daeca429ec44492b94f7d59edfd6
Karpatka2238 <101676223+Karpatka2238@users.noreply.github.com>
2022-05-22 18:02:04 +0200

    punkt 10 i punkt 11

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,70 +1,34 @@
 package pl.lublin.wsei.java.cwiczenia;
 
-
-
-import java.lang.Math;
-
-
-/*
-public class Main {
-    public static void main(String[] args) {
-        // Pkt 7.
-        System.out.print("Ala \n");
-        System.out.print("ma \n");
-        System.out.print("kota. \n");
-
-        int a =3;
-        double b = 3.45d;
-        String tekst = " jakiśtekst";
-        System.out.printf("a= %d, b= %f ,tekst = %s %n",a,b,tekst);
-
-        System.out.printf("a= %d, b= %.2f ,tekst = %20s %n",a,b,tekst);
-        System.out.printf("Nazywaliśmy to \"witaminą B3\" \n \n");
-
-        System.out.printf("alfa\tsin(alfa)\n");
-        for (int i = 0; i < 370; i+=10) {
-            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
-        }
-
-        // Math.log(logNumber)/Math.log(base);
-
-        System.out.printf("\n\n arg \t log2(arg) \t sum(arg) \n");
-        double temp =0;
-        for (int i = 0; i < 15; i+=1) {
-            int result= (int)Math.pow(2,i);
-            double suma=temp+result;
-            temp=suma;
-            System.out.printf("%d \t %.02f\t %.02f\t \n", i, Math.log(i)/Math.log(2),suma );
-        }
-
-
-    }
-
-
-
-}
-*/
-
 import java.util.Locale;
 import java.util.Scanner;
 
 
+
 public class Main {
+    public static String leftPad(String aText, char aChar, int aWidth) {
+        String res = aText;
+        for (int i = 0; i < aWidth - aText.length(); i++) {
+            res = aChar + res;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num1 =0, num2 =0;
-
+        int num1 = 0;
         do {
-            System.out.print("Podaj pierwszą liczbę: ");
+            System.out.print("Podaj liczbę: ");
             num1 = input.nextInt();
-            if (num1 != 0) {
-                System.out.print("Podaj drugą liczbę: ");
-                num2 = input.nextInt();
-            }
-            System.out.printf("Wynik dodawania %d + %d = %d ", num1, num2, num1 + num2);
 
+            String num1BIN = Integer.toBinaryString(num1);
+            num1BIN = leftPad(num1BIN, '0', 10);
 
-        } while ( num1 !=0 && num2!=0 );
+            String num1Hex = Integer.toHexString(num1);
+            num1Hex = leftPad(num1Hex, '0', 10);
 
+            System.out.printf(" „DEC = %d, BIN = %s, HEX=%s” \n", num1, num1BIN, num1Hex.toUpperCase());
+
+        } while (true);
     }
 }
